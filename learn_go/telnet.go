a81f4d03c1b87c8a0652995897fd59a427fffeea
xinkuanwu388 <xinkuanwu388@gmai.com>
2023-05-02 14:49:33 +0800

    telnet.go批量检查端口

--- learn_go/telnet.go
+++ learn_go/telnet.go
@@ -10,37 +10,31 @@ import (
 )
 
 func main() {
-	//s := []string{"5244", "5222"}
+
 	tcpGather(readFile())
 
 }
 func tcpGather(ips []string) {
 
-	//results := make(map[string]string)
 	for _, address := range ips {
-		//address := net.JoinHostPort(ip, port)
-		//fmt.Println(address)
 		// 3 秒超时
 		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
 		if err != nil {
-			//results[ip] = "failed"
 			fmt.Println("                          Fa---" + address)
 			// todo log handler
 		} else {
 			if conn != nil {
-				//results[ip] = "success"
 				fmt.Println("ok---" + address)
 				_ = conn.Close()
 			} else {
-				//results[ip] = "failed"
 				fmt.Println("                          Fa---" + address)
 			}
 		}
 	}
-	//return results
 }
 
 func readFile() []string {
+	
 	file, err := os.Open("xx.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -60,12 +54,6 @@ func readFile() []string {
 			address := strings.TrimSpace(line[9:29]) + ":" + strings.TrimSpace(line[29:])
 			lines = append(lines, address)
 		}
-
-		//lines = append(lines, strings.TrimSpace(scanner.Text()))
 	}
 	return lines
-	//fmt.Println("read lines:")
-	//for _, line := range lines {
-	//	fmt.Println(line)
-	//}
 }
